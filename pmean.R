f037d27aa64adebd85dffca6393318a8872ecf51
Mariia Khachatrian <39807453+masha711@users.noreply.github.com>
2018-06-09 23:28:58 +0300

    Create pmean.R

---
+++ pmean.R
@@ -0,0 +1,28 @@
+pmean <- function(directory, pollutant, id=1:332){
+  filenames <- sprintf("%03d.csv", id)
+  filenames <- file.path(directory, filenames)
+  my_frame <- lapply(filenames, read.csv)
+  data_frame_list <- do.call("rbind", my_frame)
+  mean(data_frame_list[[pollutant]], na.rm = TRUE)
+}
+
+complete <- function(directory, id){
+  filenames <- sprintf("%03d.csv", id)
+  filenames <- file.path(directory, filenames)
+  my_frame <- lapply(filenames, read.csv)
+  coc <- lapply(my_frame, complete.cases)
+  coc_sum <- lapply(coc, sum)
+  data.frame(id = id, nobs = I(coc_sum))
+}
+
+corr <- function(directory, threshold=0){
+  filenames <- list.files(directory, pattern="*.csv", full.names=TRUE)
+  my_frame <- lapply(filenames, read.csv)
+  result <- c()
+  for (df in my_frame){
+    if (sum(complete.cases(df)) > threshold){
+      res<-c(result, cor(df[['sulfate']], df[['nitrate']], use='pairwise.complete.obs'))
+    }
+  }
+  return(result)
+}
